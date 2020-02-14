f7851188039ba57f68e2b4e73a1efa11b4ceb5b3
Aaron Frans <aaron.frans@student.hogent.be>
2020-02-14 08:37:55 +0100

    Add project files.

---
+++ Winkel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Winkel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
